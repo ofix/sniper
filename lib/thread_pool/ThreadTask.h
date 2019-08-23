857838aa5c45f713cff840338ef9e6bc34d01666
codelighter <shb8845369@gmail.com>
2019-08-23 23:32:23 +0800

    update ThreadTask

--- lib/thread_pool/ThreadTask.h
+++ lib/thread_pool/ThreadTask.h
@@ -4,6 +4,9 @@
 #include "core/Global.h"
 #include <wx/thread.h>
 
+#define TASK_PRIORITY_HIGH 2
+#define TASK_PRIORITY_NORMAL 1
+#define TASK_PRIORITY_LOW 0
 class ThreadTask
 {
     public:
@@ -11,7 +14,9 @@ class ThreadTask
         virtual ~ThreadTask();
         virtual void Execute();
     protected:
-        int m_type;
+        int m_type; // task type
+        int m_bSuspend; // can be suspend or not
+        int m_priority; // task priority
         wxMemoryBuffer m_data;
 };
 
