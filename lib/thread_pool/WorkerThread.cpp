f76741e4fdeac1f26295d976f343991a0e9a85b1
codelighter <shb8845369@gmail.com>
2019-08-24 22:36:34 +0800

    update worker thread

--- lib/thread_pool/WorkerThread.cpp
+++ lib/thread_pool/WorkerThread.cpp
@@ -14,13 +14,26 @@ WorkerThread::~WorkerThread()
     //dtor
 }
 
+void WorkThread::SetTask(ThreadTask* pTask)
+{
+    m_pTask = pTask;
+}
+
 void*  WorkerThread::Entry()
 {
     while(1){
         // wait on m_pBossThread.m_threadsState
         if(m_pTask->type == eTask::TIMER){
             int mSpan = m_pTask->timer;
-            m_pTask->Execute(); // Execute Task
+            int iRet = m_pTask->Execute(); // Execute Task
+            // normal is 1,exception is 0 ,>1 need sleep
+            if(iRet == 0) // The thread should Sleep iRet seconds;
+            {
+
+            }else if(iRet > 1){
+                // need Sleep, ask for boss Thread wake up me iRet seconds
+            }
+
             //check for higher priority thread exist
             if(CHECK_FOR(SIGNAL_TASK_URGENT))
             {
