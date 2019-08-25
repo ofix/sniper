2025495e1e161815c3434b7c72406c47c0044d0d
codelighter <shb8845369@gmail.com>
2019-08-26 01:08:48 +0800

    revise the grammer errors in comments

--- lib/thread_pool/BossThread.h
+++ lib/thread_pool/BossThread.h
@@ -4,48 +4,51 @@
 #include <wx/thread.h>
 #include <wx/vector.h>
 
-/*****************************************************
- * Following are some beneficial think-about for thread-polling
- * If a task need keep running all the way for a fixed time span
- * ,these are many discrete, and the task is I/O bound.for such
- * program,keep the task running in UI main thread is not a good
- * idea,if only a timer used is not also a good opt,so create background
- * threads to running the task is a better way. how to pause such periodically
- * task if it wants to sleep?
- * call the system call wxThread::Sleep() in its logic loop is absolutely
- * harmful,because thread cannot awake itself if it need to run again after the
- * sleep period.
- * A wake-up mechanism needed for such situation. in the operating system kernel
- *  has a thread queue to keep trace of all thread's status, we can mimic
- * it in the user mode, for our discussion, we should make it come true in the boss-thread main loop,
- * For example,if a worker thread want to sleep, the callback
- * task function would return a number greater than 1,and copy the return value to a mutex variable
- * which are shared between boss thread and worker thread. in the meanwhile, boss thread would detect
- * this change, and push a timer to the timer-queue,the boss thread would check the timer is time-out
- * every one second(note: the minimal timer span in the boss thread).
- * Besides, we should be very careful the task priority.we should avoid a nested periodically task exist.
- * just for MySQL doesn't support nested transaction, otherwise it would make the answer complexed
- * at present.
- * some thinking-about task priority.
- * a higher priority task has the right to occupy other lower priority running worker thread's execution time.
- * if above suppose is enough, we may encounter another hazard situation: the higher priority task has not ended yet,but the lower priority thread
- * in the timer queue has timed-out,need run immediately!!! if we repeat above steps, all the worker threads would be
- * occupy by the priority execute task, and the higher priority compute bound task would be abandoned in the task queue
- * all the time.
- * If we want all the tasks has the opportunity to run,we has a opt, cut down every task execution time,just like the
- * operating system do.
- * to solve this zigzag puzzle, every task could be
-   *****
-  [timer1]
-  [timer2]
-  [timer3]
-  [timer4]
-  [timer5]
-   *****
+/*************************************************************************************************************
+ * Following are some beneficial think-about for thread pool's implementation.
+ * 1. A common application scenario *
+ *    An I/O bound task need keep running in several fixed time spans periodically, typically a user's stock program
+ * owns several background threads to pull newest stock price from remote server. the stock market
+ * has a break during 11:30 am ~ 13:00 pm, above background threads should sleep during this period.
+ * for saving CPU time. keep-alive TCP connections would be a good choice, but if all the prices come from HTTP
+ * server, no TCP connection is available, what can we do?
  *
+ *    Normally, we can create a timer in the UI main thread, to check if the sleep time for background
+ * threads has arrived. if yes, keep them SLEEP and free the worker threads occupied.
  *
+ * 2. Except above method, another possibility exist??? *
+ *    The difficulty is how to pause periodically execute tasks if they want to sleep?
+ * Calling the kernel function wxThread::Sleep() in its logic loop is absolutely useless and harmful,
+ * because sleeping thread can not awake itself automatically.
+
+ *    You may want to register a wake-up event to OS, let system to wake them up after time-out, but this
+ * requires the worker thread not do any other task during sleeping period, because OS keep a copy of
+ * all registers state before sleep and copy them back after awake.If during the sleeping time,worker thread's
+ * task are exchanged, all register value would lost, we can not avoid this step.
+
+ *    Now the only way we can choose is to mimic the OS kernel thread dispatch mechanism in our own code (the user mode),
+ * As to our problem, the best place for it is the boss-thread main loop,
  *
- *****************************************************/
+ *    For example, if a worker thread want to sleep, the task callback function would return a number greater than 1,
+ * and copy the return value to a mutex variable which are shared between boss thread and worker thread.
+ * with the OS help, the boss thread would detect this change, and push a timer with thread's ID to the timer queue,
+ * the boss thread would check the timer is time-out every one second(note: the minimal timer span in the boss thread).
+ * Besides, we should be very careful with the task priority. we should avoid nested periodically tasks exist.
+ * Just like MySQL doesn't support nested transaction, otherwise it would make the solution complexed.
+
+ * 3. The security problem about timer queue in boss thread
+ *    If a  higher priority task has the right to occupy other lower priority running worker thread's execution time.
+ * We would encounter another hazardous situation: the higher priority task has not ended yet,but the lower priority thread
+ * in the timer queue timed out and need to run immediately!!! If we let the lower priority thread awaits continually until the
+ * higher priority thread end up, this would cause severe logic error.
+ *
+ *    How to solve this zigzag puzzle? If we want all the tasks have the opportunity to run fairly, we have an opt,
+ * cut down every task execution time,just like the OS does. No matter its priority is higher or not, every task has the
+ * opportunity be seize to occupy the worker thread, and it obviously keep the worker thread busy all the time.
+ * This is not we want to see.
+ *    In a word, thread pool is efficient most of time, but not omnipotent always. We should endure its backwards.
+ * Most of time,a timer in UI main thread is a workable and better solution.
+   *************************************************************************************************************/
 class ThreadPool;
 class BossThread:public wxThread
 {
