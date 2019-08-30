d2aef3f38d1386bbdf7e9ef9ec81d972f218bc17
codelighter <shb8845369@gmail.com>
2019-08-30 23:46:46 +0800

    update WorkerThread

--- lib/thread_pool/WorkerThread.cpp
+++ lib/thread_pool/WorkerThread.cpp
@@ -5,6 +5,7 @@ WorkerThread::WorkerThread(BossThread* pBossThread,wxThreadKind kind)
                     :wxThread(kind)
                     ,m_pBossThread(pBossThread)
                     ,m_pTask(nullptr)
+                    ,m_status(WORKER_THREAD_IDLE)
 {
     //ctor
 }
@@ -19,19 +20,31 @@ void WorkThread::SetTask(ThreadTask* pTask)
     m_pTask = pTask;
 }
 
+int WorkerThread::GetStatus()
+{
+    wxMutexLocker locker(m_mutex);
+    return m_status;
+}
+
 void*  WorkerThread::Entry()
 {
     while(1){
+        wxMutexLocker locker(m_mutex);
+        m_status = WORKER_THREAD_RUNNING;
         // wait on m_pBossThread.m_threadsState
         if(m_pTask->type == eTask::TIMER){
             int mSpan = m_pTask->timer;
-            int iRet = m_pTask->Execute(); // Execute Task
+            int iSeconds = m_pTask->Execute(); // Execute Task
             // normal is 1,exception is 0 ,>1 need sleep
-            if(iRet == 0) // The thread should Sleep iRet seconds;
+            if(iSeconds == 0) // The thread should Sleep iRet seconds; task has finished
             {
-
-            }else if(iRet > 1){
+                m_status = WORKER_THREAD_IDLE;
+                break;
+            }else if(iSeconds > 1){
                 // need Sleep, ask for boss Thread wake up me iRet seconds
+                wxMutexLocker locker(m_mutex);
+                m_status = WORKER_THREAD_SLEEP;
+                wxThread::Sleep(iSeconds);
             }
 
             //check for higher priority thread exist
--- lib/thread_pool/WorkerThread.h
+++ lib/thread_pool/WorkerThread.h
@@ -8,6 +8,10 @@
 #define SIGNAL_SWAP_TASK 0x00001000
 #define SIGNAL_BACKUP_TASK 0x00010000
 
+#define WORKER_THREAD_SLEEP 2
+#define WORKER_THREAD_RUNNING 1
+#define WORKER_THREAD_IDLE 0
+
 class BossThread;
 class WorkerThread:public wxThread
 {
@@ -15,10 +19,13 @@ class WorkerThread:public wxThread
         WorkerThread(BossThread* pBossThread,wxThreadKind kind=wxTHREAD_DETACHED);
         virtual ~WorkerThread();
         virtual void* Entry();
+        int GetStatus();
 
     protected:
         BossThread* m_pBossThread;
         ThreadTask* m_pTask;
+        int m_status;
+        wxMutex m_mutex;
 };
 
 #endif // WorkerThread_H
