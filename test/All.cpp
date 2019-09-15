5de5e8bfcd02ca10c0b942e112a221f105f185bc
codelighter <shb8845369@gmail.com>
2019-09-15 20:59:18 +0800

    add symmetry number helper functions

--- test/All.cpp
+++ test/All.cpp
@@ -1,10 +1,15 @@
 #include "All.h"
 #include "include/tool/MarkDown.h"
 #include "include/util/Helper.h"
+#include <iomanip>
 
 void Test::RunAll()
 {
+    #ifdef xx
     mark_down();
+    #else
+    symmetry_nums();
+    #endif
 }
 
 void Test::mark_down()
@@ -12,4 +17,50 @@ void Test::mark_down()
     wxString strExe = getExecDir();
     MarkDown md(strExe+_T("install.txt"),strExe+_T("install.md"));
     md.CsvToMd();
+}
+
+int Test::symmetry_nums()
+{
+    int sum=0;
+    for(int i=0xF000; i<=0xFFFF; i++){// if i is uint8_t here, would overflow after 255 and cause dead loop!!!
+        if(is_num_symmetry((uint16_t)i)){
+            sum++;
+            std::cout<<"######  "<<std::setw(3)<<i;
+            std::cout<<"  "<<numToBits((uint16_t)i)<<std::endl;
+        }else{
+            std::cout<<"XXX  "<<std::setw(3)<<i;
+            std::cout<<"  "<<numToBits((uint16_t)i)<<std::endl;
+        }
+    }
+    std::cout<<"### total symmetry numbers: "<<sum<<std::endl;
+    return sum;
+}
+
+template <typename T>
+std::string Test::numToBits(T n){
+    uint32_t nBytes = sizeof(n);
+    int bits = nBytes*8-1;
+    std::string s="";
+    while(bits>=0){
+        s += ((n>>bits)&1) == 1? "1":"0";
+        bits --;
+    }
+    return s;
+}
+
+template <typename T>
+bool Test::is_num_symmetry(T num)
+{
+    uint32_t n = sizeof(num)*8;
+    uint32_t mid = n/2;
+    uint32_t M = n;
+    while(n>=mid)
+    {
+        if((((num>>n)&1)!=((num>>(M-1-n))&1)))
+        {
+            return false;
+        }
+        n--;
+    }
+    return true;
 }
--- test/All.h
+++ test/All.h
@@ -1,10 +1,17 @@
 #ifndef ALL_H
 #define ALL_H
 
+#include <string>
+
 namespace Test
 {
     void RunAll();
     void mark_down();
+    int symmetry_nums();
+    template<typename T>
+    std::string numToBits(T n);
+    template<typename T>
+    bool is_num_symmetry(T num);
 }
 
 #endif // ALL_H
